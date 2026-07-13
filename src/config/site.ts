4bda060f5b2030720b753078f676e7d29f5c0fc6
Ivan Cvetkovic <i.cvetkovic994@gmail.com>
2026-07-13 16:36:15 +0200

    Enhance business structured data

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,2 +1,11 @@
 export const SITE_URL = 'https://cortexflow.technology';
 export const SITE_LOGO_URL = `${SITE_URL}/logo-mark.svg`;
+export const BUSINESS_EMAIL = 'dbckconsult@gmail.com';
+export const BUSINESS_PHONE = '+381 64 2311336';
+export const BUSINESS_ADDRESS = {
+  '@type': 'PostalAddress',
+  streetAddress: 'Stanoja Bunuševca 47',
+  addressLocality: 'Niš',
+  postalCode: '18000',
+  addressCountry: 'RS'
+};
