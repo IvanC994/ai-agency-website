eff41c987371bcb5d02fb219e71b49321a238170
Ivan Cvetkovic <i.cvetkovic994@gmail.com>
2026-07-21 09:17:03 +0200

    Clarify brand and legal entity

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,13 @@
 export const SITE_URL = 'https://routineforge.tech';
 export const SITE_LOGO_URL = `${SITE_URL}/logo-mark.png`;
+export const BRAND_NAME = 'RoutineForge';
+export const BUSINESS_LEGAL_NAME = 'Ivan Cvetković PR Agencija DBCK Konsalting';
+export const BUSINESS_ALTERNATE_NAME = 'DBCK Consulting Agency';
 export const BUSINESS_EMAIL = 'office@routineforge.tech';
 export const BUSINESS_PHONE = '+381 64 2311336';
+export const BUSINESS_TAX_ID = '113866243';
+export const BUSINESS_REGISTRATION_NUMBER = '67143523';
+export const BUSINESS_LINKEDIN_URL = 'https://www.linkedin.com/company/routineforge';
 export const BUSINESS_ADDRESS = {
   '@type': 'PostalAddress',
   streetAddress: 'Stanoja Bunuševca 47',
