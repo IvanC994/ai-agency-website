01dfd0ff38e9c05315fa5df55d1f6603fcd595d8
Ivan Cvetkovic <i.cvetkovic994@gmail.com>
2026-07-28 16:13:52 +0200

    Add post-enquiry consultation booking

---
+++ src/config/booking.js
@@ -0,0 +1,2 @@
+export const BOOKING_URL = 'https://calendar.app.google/XkHAb2XiNn7c8ZMWA';
+export const BOOKING_EMBED_URL = 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ0umvncgkth7wGUc3E0FWGoM4FR62-FUaPD_5qJwOQ5uuhzE5CDPXYYLgdpLFqmTMcTvxFlYRuZ?gv=true';
